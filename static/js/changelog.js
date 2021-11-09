f86f95ff60156ff9fb2c15060951be1d43ceb1ac
cburke <cburke@theediproject.com>
2021-11-09 12:20:04 -0800

    Added changelog as a tab.

---
+++ static/js/changelog.js
@@ -0,0 +1,7 @@
+const CHANGES = [
+    {Date: "2021-11-09", Changes: ['Added this change log.','Added Markov chain name generation (50% chance) for first and last names.','Improved name list.']},
+    {Date: "2021-10-30", Changes: ['Warn users when no Common race is selected.']},
+    {Date: "2021-10-28", Changes: ['Dark styling.','Removed random descriptions related to prosperity and natural features.','Improved natural features and improved logic for business generation.']},
+    {Date: "2021-07-20", Changes: ['Added prosperity options.']},
+    {Date: " Older", Changes: ['A lot to document, all very boring.']},
+];
